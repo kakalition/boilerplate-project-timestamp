94f4446a3cf50e53151a3a29059597aed0480b46
Kaka <kakalition@gmail.com>
2024-07-22 14:37:41 +0700

    patch

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/:date", function (req, res) {
+app.get("/api/:date?", function (req, res) {
   const rawDate = req.params['date'];
 
   if (!rawDate) {
